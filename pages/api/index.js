9d52c8b88bf091e331beed07c838784a312cf23a
murikao <mauricioplg@bol.com.br>
2020-11-27 16:20:23 -0300

    alterei resp api e outros fixs

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   
   res.statusCode = 200
   var ip = req.headers['x-forwarded-for'] || 
@@ -6,8 +6,15 @@ export default function handler(req, res) {
      req.socket.remoteAddress ||
      (req.connection.socket ? req.connection.socket.remoteAddress : null);
      ip = ip.match(/\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}/)
-  console.log(">>>>>>>api/"+ip)
+     const ip4=ip.toString().match(/192\.168/)=="192.168"?"":ip
+     const resip = await fetch(`https://king.host/wiki/wp-content/themes/kinghost-wiki/includes/ip-api.php?ip=${ip4}`)
+     const data = await resip.json()
+   
+     var resp ={
+    cidade: data.city, uf: data.region, pais:data.country, paiscod:data.countryCode, 
+     Operadora : data.isp, ip: ip[0]}
+  console.log(">>>>>>>api/"+resp)
   res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ "ip":ip[0]}))
+  res.end(JSON.stringify(resp))
   //res.send(req.toString())
 }
